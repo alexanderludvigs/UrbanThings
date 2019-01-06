073023b230f1fd61d9b524a3369f2ff9381acfae
alexander <alexander@reontech.com>
2019-01-06 22:30:51 +0000

    Initial Commit

---
+++ UrbanThings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UrbanThings
+//
+//  Created by Alexander on 06/01/2019.
+//  Copyright © 2019 alexanderludvigs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
